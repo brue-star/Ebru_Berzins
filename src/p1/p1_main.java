be968c407099b93619d02e418542a31cc49cc28b
brue-star <hallo.brue@gmail.com>
2025-10-27 11:26:57 +0100

    p1/P1_main: Scanner-Echo

---
+++ src/p1/p1_main.java
@@ -0,0 +1,16 @@
+package p1;
+
+import java.util.Scanner;
+
+public class p1_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String zeile = new String();
+		zeile = s.nextLine();
+		System.out.println(zeile);
+		s.close();
+	}
+
+}
